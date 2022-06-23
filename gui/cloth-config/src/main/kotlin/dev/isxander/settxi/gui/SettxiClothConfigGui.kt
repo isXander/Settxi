2554dfaafecb20af705a591da16e2f4437b45a71
xander <xander@isxander.dev>
2022-06-23 19:13:34 +0100

    fix ranges for cloth gui

--- gui/cloth-config/src/main/kotlin/dev/isxander/settxi/gui/SettxiClothConfigGui.kt
+++ gui/cloth-config/src/main/kotlin/dev/isxander/settxi/gui/SettxiClothConfigGui.kt
@@ -8,6 +8,8 @@ import me.shedaniel.clothconfig2.api.ConfigEntryBuilder
 import me.shedaniel.clothconfig2.impl.builders.EnumSelectorBuilder
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.text.Text
+import java.io.File
+import java.util.Optional
 import java.util.function.Supplier
 
 /**
@@ -40,38 +42,64 @@ private fun ConfigBuilder.entry(setting: Setting<*>) =
                 defaultValue = Supplier { setting.default }
                 setTooltip(Text.translatable(setting.description))
                 setSaveConsumer { setting.set(it) }
-                setMin(setting.range.start)
-                setMax(setting.range.endInclusive)
+                if (setting.range != null) {
+                    setMin(setting.range!!.start)
+                    setMax(setting.range!!.endInclusive)
+                }
             }
         is FloatSetting ->
             entryBuilder().startFloatField(Text.translatable(setting.name), setting.get()).apply {
                 defaultValue = Supplier { setting.default }
                 setTooltip(Text.translatable(setting.description))
                 setSaveConsumer { setting.set(it) }
-                setMin(setting.range.start)
-                setMax(setting.range.endInclusive)
+                if (setting.range != null) {
+                    setMin(setting.range!!.start)
+                    setMax(setting.range!!.endInclusive)
+                }
             }
         is LongSetting ->
-            entryBuilder().startLongSlider(
-                Text.translatable(setting.name),
-                setting.get(),
-                setting.range.first,
-                setting.range.last
-            ).apply {
-                defaultValue = Supplier { setting.default }
-                setTooltip(Text.translatable(setting.description))
-                setSaveConsumer { setting.set(it) }
+            if (setting.range != null) {
+                entryBuilder().startLongSlider(
+                    Text.translatable(setting.name),
+                    setting.get(),
+                    setting.range!!.first,
+                    setting.range!!.last
+                ).apply {
+                    defaultValue = Supplier { setting.default }
+                    setTooltip(Text.translatable(setting.description))
+                    setSaveConsumer { setting.set(it) }
+                }
+            } else {
+                entryBuilder().startLongField(
+                    Text.translatable(setting.name),
+                    setting.get(),
+                ).apply {
+                    defaultValue = Supplier { setting.default }
+                    setTooltip(Text.translatable(setting.description))
+                    setSaveConsumer { setting.set(it) }
+                }
             }
         is IntSetting ->
-            entryBuilder().startIntSlider(
-                Text.translatable(setting.name),
-                setting.get(),
-                setting.range.first,
-                setting.range.last
-            ).apply {
-                defaultValue = Supplier { setting.default }
-                setTooltip(Text.translatable(setting.description))
-                setSaveConsumer { setting.set(it) }
+            if (setting.range != null) {
+                entryBuilder().startIntSlider(
+                    Text.translatable(setting.name),
+                    setting.get(),
+                    setting.range!!.first,
+                    setting.range!!.last
+                ).apply {
+                    defaultValue = Supplier { setting.default }
+                    setTooltip(Text.translatable(setting.description))
+                    setSaveConsumer { setting.set(it) }
+                }
+            } else {
+                entryBuilder().startIntField(
+                    Text.translatable(setting.name),
+                    setting.get(),
+                ).apply {
+                    defaultValue = Supplier { setting.default }
+                    setTooltip(Text.translatable(setting.description))
+                    setSaveConsumer { setting.set(it) }
+                }
             }
         is StringSetting ->
             entryBuilder().startStrField(Text.translatable(setting.name), setting.get()).apply {
@@ -97,7 +125,17 @@ private fun ConfigBuilder.entry(setting: Setting<*>) =
         is EnumSetting<*> ->
             setting.toEnumSelector(entryBuilder())
         is FileSetting ->
-            throw UnsupportedOperationException("Cloth config does not support file settings")
+            entryBuilder().startStrField(Text.translatable(setting.name), setting.get().path).apply {
+                defaultValue = Supplier { setting.default.path }
+                setTooltip(Text.translatable(setting.description))
+                setSaveConsumer { setting.set(File(it)) }
+                setErrorSupplier {
+                    if (!File(it).exists())
+                        Optional.of(Text.translatable("settxi.cloth.file_not_exists"))
+                    else
+                        Optional.empty()
+                }
+            }
 
         else -> throw IllegalArgumentException("Unknown setting type: ${setting.javaClass}")
     }.build()
